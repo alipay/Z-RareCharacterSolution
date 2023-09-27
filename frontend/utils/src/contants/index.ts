8cba8195ac68590faaa3fbedb350e10435a0e363
xingying <dikang.lidikang@alibaba-inc.com>
2023-09-27 11:36:51 +0800

    feat: 补充1486个字

--- frontend/utils/src/contants/index.ts
+++ frontend/utils/src/contants/index.ts
@@ -1,7 +1,7 @@
 /**
  * 默认字体下载地址
  */
-export const DEFAULT_FONT_URL = 'https://mdn.alipayobjects.com/huamei_seif62/afts/file/A*63IqSLzYAdEAAAAAAAAAAAAADh18AQ/RareWordsFonts-v1.0.14.ttf';
+export const DEFAULT_FONT_URL = 'https://mdn.alipayobjects.com/huamei_2fq7mt/afts/file/A*wBbQRYCq_p4AAAAAAAAAAAAADh58AQ/RareWordsFonts.ttf';
 
 /**
  * 字库缓存key值
